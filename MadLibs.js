46821e09e66d39b5035e721c8fb6d6f6f111e1fb
Sergio Almonte <sergioealmonte@gmail.com>
2019-03-04 17:48:00 -0800

    final page design

--- MadLibs.js
+++ MadLibs.js
@@ -1,1 +1,11 @@
-window.prompt = 'What did you eat today?';
+let Animal = window.prompt ('What is your favorite animal?.');
+document.getElementById('Animal').innerText = Animal; 
+let Time = window.prompt ('What time do you usually wake up?');
+document.getElementById('Time').innerText = Time;
+let Location = window.prompt ('Where is your favorite place to go?');
+document.getElementById('Location').innerText = Location;
+let Activity = window.prompt ('What is your favorite thing to do? (Jogging, Cooking, etc...');
+document.getElementById('Activity').innerText = Activity;
+let food = window.prompt('What is your favorite food?');
+document.getElementById('food').innerText = food;
+
