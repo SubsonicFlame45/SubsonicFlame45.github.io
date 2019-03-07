14a034b3c976393300e306ba7fd36ae70f0d9c85
Sergio Almonte <sergioealmonte@gmail.com>
2019-03-06 16:59:59 -0800

    Added buttons to my mad libs

--- MadLibs.js
+++ MadLibs.js
@@ -1,11 +1,27 @@
-let Animal = window.prompt ('What is your favorite animal?.');
+function changeAnimal() {
+    let Animal = window.prompt ('What is your favorite animal?.');
 document.getElementById('Animal').innerText = Animal; 
-let Time = window.prompt ('What time do you usually wake up?');
+}
+function changeTime() {
+    let Time = window.prompt ('What time do you usually wake up?');
 document.getElementById('Time').innerText = Time;
-let Location = window.prompt ('Where is your favorite place to go?');
+}
+function changeLocation() {
+    let Location = window.prompt ('Where is your favorite place to go?');
 document.getElementById('Location').innerText = Location;
-let Activity = window.prompt ('What is your favorite thing to do? (Jogging, Cooking, etc...');
+}
+function changeActivity()  {
+    let Activity = window.prompt ('What is your favorite thing to do? (Jogging, Cooking, etc...');
 document.getElementById('Activity').innerText = Activity;
-let food = window.prompt('What is your favorite food?');
+}
+function changeFood() {
+    let food = window.prompt('What is your favorite food?');
 document.getElementById('food').innerText = food;
+}
+document.getElementById('change-animal-button').onclick = changeAnimal
+document.getElementById('change-time-button').onclick = changeTime
+document.getElementById('change-location-button').onclick = changeLocation
+document.getElementById('change-activity-button').onclick = changeActivity
+document.getElementById('change-food-button').onclick = changeFood
 
+console.log('This is working');
