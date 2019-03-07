74bc080f17cd607acedaae3baa965629cdf18775
Sergio Almonte <sergioealmonte@gmail.com>
2019-03-06 17:51:51 -0800

    hdhd

--- MadLibs.js
+++ MadLibs.js
@@ -1,5 +1,5 @@
 function changeAnimal() {
-    let Animal = window.prompt ('What is your favorite animal?.');
+    let Animal = window.prompt ('What is your favorite animal?');
 document.getElementById('Animal').innerText = Animal; 
 }
 function changeTime() {
@@ -18,10 +18,25 @@ function changeFood() {
     let food = window.prompt('What is your favorite food?');
 document.getElementById('food').innerText = food;
 }
+function changeName() {
+    let name = window.prompt ('What do you want to name your animal?');
+    document.getElementById('name').innerText = name;
+}
+function changePronoun() {
+    let pronoun = window.prompt('Is your animal a he or she?');
+    console.log(pronoun);
+    let pronounElements = document.querySelectorAll('.pronoun')
+    for (let i = 0; i < pronounElements.length; i++)  {
+        pronounElements[i].innerText = pronoun;
+    }
+}
+
 document.getElementById('change-animal-button').onclick = changeAnimal
+document.getElementById('change-name-button').onclick = changeName
 document.getElementById('change-time-button').onclick = changeTime
 document.getElementById('change-location-button').onclick = changeLocation
 document.getElementById('change-activity-button').onclick = changeActivity
 document.getElementById('change-food-button').onclick = changeFood
+document.getElementById('change-pronoun-button').onclick = changePronoun
 
 console.log('This is working');
