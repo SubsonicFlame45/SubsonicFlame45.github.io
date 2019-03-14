99ec3e2b34b3ff37e8f0484e54842644136a3628
Sergio Almonte <sealmonte@s.sfusd.edu>
2019-03-13 17:54:40 -0700

    added cooridnates

--- flower-challenge.js
+++ flower-challenge.js
@@ -1,6 +1,6 @@
 //var draw = SVG('drawing').size(150, 150);
 //var rect = draw.circle(60).fill('purple').move(20, 20); 
-
+//^^^ for drawing basic circles, edit draw.circle() to change size
 
 function flower() {
     var draw = SVG('drawing').size(150, 150);
