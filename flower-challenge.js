4b2a53263180dc180ddd4c3ebe68c176e8ee22e2
Sergio Almonte <sealmonte@s.sfusd.edu>
2019-03-13 17:52:17 -0700

    started my flower challenge

---
+++ flower-challenge.js
@@ -0,0 +1,10 @@
+//var draw = SVG('drawing').size(150, 150);
+//var rect = draw.circle(60).fill('purple').move(20, 20); 
+
+
+function flower() {
+    var draw = SVG('drawing').size(150, 150);
+    var rect = draw.circle(100).fill('#f06').move(20, 20)
+}
+
+document.getElementById('start-button').onclick
