0f06b44133fb66e70211addd55e84d64983f89d9
Sergio Almonte <sergioealmonte@gmail.com>
2019-03-18 17:58:16 -0700

    d

--- flower-challenge.js
+++ flower-challenge.js
@@ -1,10 +1,29 @@
-//var draw = SVG('drawing').size(150, 150);
-//var rect = draw.circle(60).fill('purple').move(20, 20); 
-//^^^ for drawing basic circles, edit draw.circle() to change size
 
-function flower() {
-    var draw = SVG('drawing').size(150, 150);
-    var rect = draw.circle(100).fill('#f06').move(20, 20)
+var draw = SVG('drawing').size(1500, 900);
+
+function flower() {   
+    drawFlower(110, 100);
+    drawFlower(300, 100);
+    drawFlower(500, 100);
+    drawFlower(90, 250);
+    drawFlower(350, 330);
+    drawFlower(500, 300);
+    drawFlower(550, 500);
+    drawFlower(100, 390);
+    drawFlower(240, 460);
+    
+
+}
+function drawFlower(x, y) {
+    let circle = draw.circle(60).fill('#ff9215').center(x, y)
+    let ellipse = draw.ellipse(40, 20).fill('#c627bb').center(x - 20, y - 20)
+    let ellipse2 = draw.ellipse(40, 20).fill('#c627bb').center(x - 25, y)
+    let ellipse3 = draw.ellipse(40, 20).fill('#c627bb').center(x - 20, y + 20)
+    let ellipse4 = draw.ellipse(20,40).fill('#c627bb').center(x, y + 25)
+    let ellipse5 = draw.ellipse(20,40).fill('#c627bb').center(x, y - 25)
+    let ellipse6 = draw.ellipse(40,20).fill('#c627bb').center(x + 20, y + 20)
+
+
 }
 
-document.getElementById('start-button').onclick
+document.getElementById('start-button').onclick = flower;
