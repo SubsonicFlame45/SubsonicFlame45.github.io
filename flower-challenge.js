d464e5dad31c2cf3689bfba8e4897435c42fc624
Sergio Almonte <sergioealmonte@gmail.com>
2019-03-20 16:27:07 -0700

    finished part one of flower challenge

--- flower-challenge.js
+++ flower-challenge.js
@@ -3,7 +3,7 @@ var draw = SVG('drawing').size(1500, 900);
 
 function flower() {   
     drawFlower(110, 100);
-    drawFlower(300, 100);
+    drawFlower(300, 90);
     drawFlower(500, 100);
     drawFlower(90, 250);
     drawFlower(350, 330);
@@ -11,19 +11,23 @@ function flower() {
     drawFlower(550, 500);
     drawFlower(100, 390);
     drawFlower(240, 460);
-    
-
 }
 function drawFlower(x, y) {
-    let circle = draw.circle(60).fill('#ff9215').center(x, y)
+    var line = draw.line(0, 75, 0, 0).move(x , y)
+    line.stroke({ color: '#000000', width: 6, linecap: 'square' })
     let ellipse = draw.ellipse(40, 20).fill('#c627bb').center(x - 20, y - 20)
+    ellipse.transform({rotation: 30});
     let ellipse2 = draw.ellipse(40, 20).fill('#c627bb').center(x - 25, y)
     let ellipse3 = draw.ellipse(40, 20).fill('#c627bb').center(x - 20, y + 20)
+    ellipse3.transform({rotation: 330});
     let ellipse4 = draw.ellipse(20,40).fill('#c627bb').center(x, y + 25)
     let ellipse5 = draw.ellipse(20,40).fill('#c627bb').center(x, y - 25)
     let ellipse6 = draw.ellipse(40,20).fill('#c627bb').center(x + 20, y + 20)
-
-
+    ellipse6.transform({rotation: 30});
+    let ellispe7 = draw.ellipse(40,20).fill('#c627bb').center(x + 20, y)
+    let ellispe8 = draw.ellipse(40,20).fill('#c627bb').center(x + 20, y - 20)
+    ellispe8.transform({rotation: 150});
+    let circle = draw.circle(60).fill('#ff9215').center(x, y)
 }
 
 document.getElementById('start-button').onclick = flower;
