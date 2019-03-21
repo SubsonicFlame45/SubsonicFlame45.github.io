3cf259dac28aa9d46873ec43f31a0c343c244c66
Sergio Almonte <sergioealmonte@gmail.com>
2019-03-20 17:54:59 -0700

    getting ready for animation

--- flower-challenge.js
+++ flower-challenge.js
@@ -28,26 +28,49 @@ function drawFlower(x, y) {
     let ellispe8 = draw.ellipse(40,20).fill('#c627bb').center(x + 20, y - 20)
     ellispe8.transform({rotation: 150});
     let circle = draw.circle(60).fill('#ff9215').center(x, y)
+    circle.click(function() {
+        console.log("success");
+        console.log(x, y);
+      })
+}
+function moveBee(x, y) {
+    rect.animate().move(x, y)
 }
 function bee() {
-    drawBee(680, 40);
+    // drawBee(680, 40);
+    var bee = drawBee(680, 40)
 }
+
+
 function drawBee(x , y) {
     let ellipse = draw.ellipse(45 , 25).fill('#ecd100').center(x, y)
     var line = draw.line(0, 12, 0, 0).move(x , y)
     line.stroke({ color: '#000000', width: 2, linecap:'round'})
-    var line = draw.line(0, 11, 0, 0).move(x - 7, y)
-    line.stroke({ color: '#000000', width: 2, linecap:'round'})
-    var line = draw.line(0, 11, 0, 0).move(x + 7, y)
-    line.stroke({ color: '#000000', width: 2, linecap:'round'})
-    var line = draw.line(0, 13, 0, 0).move(x + 15, y - 4)
-    line.stroke({ color: '#000000', width: 2, linecap:'round'})
+    var line2 = draw.line(0, 11, 0, 0).move(x - 7, y)
+    line2.stroke({ color: '#000000', width: 2, linecap:'round'})
+    var line3 = draw.line(0, 11, 0, 0).move(x + 7, y)
+    line3.stroke({ color: '#000000', width: 2, linecap:'round'})
+    var line4 = draw.line(0, 13, 0, 0).move(x + 15, y - 4)
+    line4.stroke({ color: '#000000', width: 2, linecap:'round'})
     let ellispe2 = draw.ellipse(40, 17).fill('#4dd2cf').center(x + 5, y - 10)
     ellispe2.transform({rotation: 350})
     let circle = draw.circle(18).fill('#000000').center(x - 20, y)
-    var line = draw.line(0, 0, 8, 8).move(x - 35, y - 15)
-    line.stroke({ color: '#000000', width: 3, linecap:'square'})
+    var line5 = draw.line(0, 0, 8, 8).move(x - 35, y - 15)
+    line5.stroke({ color: '#000000', width: 3, linecap:'square'})
     let circle1 = draw.circle(5).fill('#ec0700').center(x - 22, y)
+    var bee = draw().group;
+    bee.add(ellipse);
+    bee.add(ellipse2);
+    bee.add(line);
+    bee.add(line2);
+    bee.add(line3);
+    bee.add(line4);
+    bee.add(line5);
+    bee.add(circle);
+    bee.add(circle1);
+    return(bee)
+
 }
+
 document.getElementById('start-button').onclick = flower;
 document.getElementById('bee-button').onclick = bee;
